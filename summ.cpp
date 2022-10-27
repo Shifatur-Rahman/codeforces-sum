d5ffad0563ad4a3192a15adff79c8ec4a1da6252
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2022-10-27 06:01:36 +0600

    Add files via upload

---
+++ summ.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int t, a, b, c;
+cin>>t;
+while(t--){
+	cin>>a>>b>>c;
+	if((a+b == c)|| (a+c == b) || (b+c==a)){
+	cout<<"YES"<<endl;
+	} else{
+	cout<<"NO"<<endl;
+	}
+}
+
+
+
+}
